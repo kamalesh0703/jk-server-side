147a19217d72b5303fda9d3658c9ed0149ca6942
kamalesh0703 <kamaleshponmani0703@gmai.com>
2023-09-11 23:55:55 +0530

    sqasaa

--- server-side/DataBase/DataBase.js
+++ server-side/DataBase/DataBase.js
@@ -1,3 +1,3 @@
 module.exports={
-    db:'mongodb+srv://kamaleshponmani0703:7rxiriod64@cluster0.uwqbxli.mongodb.net/'
+    db:'mongodb+srv://kamalesh0703:7rxiriod64@cluster0.kkp3jn7.mongodb.net/?retryWrites=true&w=majority'
 }
